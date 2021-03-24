05d36711844c5a945b17f81c8c0345de9d36d7d5
Colton Medlin <coltonmedlin@gmail.com>
2021-03-23 20:02:19 -0700

    CSV: basic setup of server -- also added nodemon

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('this works yooooo!!!');
+});
+
+app.listen(port, () => {
+  console.log(`CSV report generator is listening at http:localhost:${port}`);
+});
 
