89b99cc0943ed932771a7b52fb5780a8fe8bd7cc
Colton Medlin <coltonmedlin@gmail.com>
2021-04-13 20:43:40 -0700

    MultiForm: setup react properly

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,22 @@
 
 
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render () {
+    return(
+     <h1>TEST!</h1>
+    )
+  }
+
+}
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 1111;
 
+app.use(express.static(__dirname + '/public/'));
+
+app.listen(port, () => {
+  console.log(`multiform is running at port:${port}`);
+})
 
